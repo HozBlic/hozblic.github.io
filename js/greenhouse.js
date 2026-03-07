23b3beca2fb0f9c3606ec8d59092553d11432f88
HozBlic <alise.daine99@gmail.com>
2026-03-07 22:11:03 +0200

    greenhouse scroll and favicon issue

--- js/greenhouse.js
+++ js/greenhouse.js
@@ -1,4 +1,28 @@
 function loadMenuItems() {
+
+    
+    let bolScrolled = false;
+    let intPrevScrollPos = 0;
+
+    $('.tab_content').on('scroll', function () {
+        bolScrolled = true;
+    })
+
+    setInterval(function () {
+        if (bolScrolled) {
+            bolScrolled = false;
+            var intCurrentScrollPos = $('.tab_content:visible').scrollTop();
+            if (intPrevScrollPos < intCurrentScrollPos && intCurrentScrollPos > 500) {
+                if (intCurrentScrollPos - intPrevScrollPos > 30) {
+                    $('#header').addClass('hidden');
+                }
+            } else {
+                $('#header').removeClass('hidden');
+            }
+            intPrevScrollPos = intCurrentScrollPos;
+        }
+    }, 150);
+    
     $('#side_menu #title .version').text(`v${objBuild.version}`);
 
     var arrModes = ['mode_dark', 'mode_name', 'mode_gift', 'mode_collapse', 'mode_chbexpand', 'mode_spoilers', 'mode_mini', 'mode_disable_tooltip', 'mode_mini_tooltip'];
